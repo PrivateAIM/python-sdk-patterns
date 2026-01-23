ca3e30470c2fba00e30d22f28a8259ed3a47d324
Nightknight3000 <alexander.roehl@uni-tuebingen.de>
2026-01-23 12:02:04 +0100

    feat: update local dp pattern (#5)
    * feat: update local dp pattern
    
    * fix: add local dp variables to mock tester
    
    * feat: enhance local differential privacy support for local testing
    
    * feat: add custom kwargs support for aggregator instance in convergence test
    
    * fix: refine convergence criteria handling in aggregation process
    
    * feat: implement custom analyzer and aggregator for local differential privacy model
    
    ---------
    
    Co-authored-by: antidodo <albin2993@gmail.com>
    Co-authored-by: davidhieber <david.hieber@uni-tuebingen.de>

--- flame/star/star_localdp/star_localdp_model.py
+++ flame/star/star_localdp/star_localdp_model.py
@@ -1,13 +1,17 @@
-from typing import Optional, Type, Literal, Union
+from typing import Optional, Type, Literal, Union, Any
 
 from flamesdk import FlameCoreSDK
 from flame.star.aggregator_client import Aggregator
 from flame.star.analyzer_client import Analyzer
 from flame.star.star_model import StarModel, _ERROR_MESSAGES
+from flame.utils.mock_flame_core import MockFlameCoreSDK
 
 
 class StarLocalDPModel(StarModel):
-    flame: FlameCoreSDK
+    flame: Union[FlameCoreSDK, MockFlameCoreSDK]
+
+    data: Optional[list[dict[str, Any]]] = None
+    test_mode: bool = False
 
     epsilon: Optional[float]
     sensitivity: Optional[float]
@@ -22,7 +26,11 @@ class StarLocalDPModel(StarModel):
                  analyzer_kwargs: Optional[dict] = None,
                  aggregator_kwargs: Optional[dict] = None,
                  epsilon: Optional[float] = None,
-                 sensitivity: Optional[float] = None) -> None:
+                 sensitivity: Optional[float] = None,
+                 test_mode: bool = False,
+                 test_kwargs: Optional[dict] = None) -> None:
+        self.epsilon = epsilon
+        self.sensitivity = sensitivity
         super().__init__(analyzer=analyzer,
                          aggregator=aggregator,
                          data_type=data_type,
@@ -30,52 +38,61 @@ class StarLocalDPModel(StarModel):
                          simple_analysis=simple_analysis,
                          output_type=output_type,
                          analyzer_kwargs=analyzer_kwargs,
-                         aggregator_kwargs=aggregator_kwargs)
-        self.epsilon = epsilon
-        self.sensitivity = sensitivity
+                         aggregator_kwargs=aggregator_kwargs,
+                         test_mode=test_mode,
+                         test_kwargs=test_kwargs)
 
     def _start_aggregator(self,
                           aggregator: Type[Aggregator],
                           simple_analysis: bool = True,
                           output_type: Literal['str', 'bytes', 'pickle'] = 'str',
-                          aggregator_kwargs: Optional[dict] = None) -> None:
-        if self._is_aggregator():
-            if issubclass(aggregator, Aggregator):
-                # init custom aggregator subclass
-                if aggregator_kwargs is None:
-                    aggregator = aggregator(flame=self.flame)
-                else:
-                    aggregator = aggregator(flame=self.flame, **aggregator_kwargs)
-
-                # Ready Check
-                self._wait_until_partners_ready()
-
-                # Get analyzer ids
-                analyzers = aggregator.partner_node_ids
-
-                while not self._converged():  # (**)
-                    # Await intermediate results
-                    result_dict = self.flame.await_intermediate_data(analyzers)
-
-                    # Aggregate results
-                    agg_res, converged = aggregator.aggregate(list(result_dict.values()), simple_analysis)
-                    self.flame.flame_log(f"Aggregated results: {str(agg_res)[:100]}")
-
-                    if converged:
-                        self.flame.flame_log("Submitting final results using differential privacy...", end='')
-                        if self.epsilon and self.sensitivity:
-                            localdp = {"epsilon": self.epsilon, "sensitivity": self.sensitivity}
-                        else:
-                            localdp = None
-                        response = self.flame.submit_final_result(agg_res, output_type, localdp=localdp)
-                        self.flame.flame_log(f"success (response={response})")
-                        self.flame.analysis_finished()  # LOOP BREAK
-                    else:
-                        # Send aggregated result to analyzers
-                        self.flame.send_intermediate_data(analyzers, agg_res)
-
-                aggregator.node_finished()
+                          aggregator_kwargs: Optional[dict] = None,
+                          test_node_kwargs: Optional[dict[str, Any]] = None) -> None:
+        if issubclass(aggregator, Aggregator):
+            # init custom aggregator subclass
+            if aggregator_kwargs is None:
+                aggregator = aggregator(flame=self.flame)
             else:
-                raise BrokenPipeError(_ERROR_MESSAGES.IS_INCORRECT_CLASS.value)
+                aggregator = aggregator(flame=self.flame, **aggregator_kwargs)
+
+            if test_node_kwargs is not None:
+                aggregator.set_num_iterations(test_node_kwargs['num_iterations'])
+                aggregator.set_latest_result(test_node_kwargs['latest_result'])
+
+            # Ready Check
+            self._wait_until_partners_ready()
+
+            # Get analyzer ids
+            analyzers = aggregator.partner_node_ids
+
+            while not aggregator.finished:  # (**)
+                # Await intermediate results
+                result_dict = self.flame.await_intermediate_data(analyzers)
+
+                # Aggregate results
+                agg_res, converged, delta_crit = aggregator.aggregate(list(result_dict.values()), simple_analysis)
+                self.flame.flame_log(f"Aggregated results: {str(agg_res)[:100]}")
+
+                if converged:
+                    if not self.test_mode:
+                        self.flame.flame_log("Submitting final results using differential privacy...",
+                                             log_type='info',
+                                             end='')
+                    if delta_crit and (self.epsilon is not None) and (self.sensitivity is not None):
+                        local_dp = {"epsilon": self.epsilon, "sensitivity": self.sensitivity}
+                    else:
+                        local_dp = None
+                    if self.test_mode and (local_dp is not None):
+                        self.flame.flame_log(f"\tTest mode: Would apply local DP with epsilon={local_dp['epsilon']} "
+                                             f"and sensitivity={local_dp['sensitivity']}",
+                                             log_type='info')
+                    response = self.flame.submit_final_result(agg_res, output_type, local_dp=local_dp)
+                    if not self.test_mode:
+                        self.flame.flame_log(f"success (response={response})", log_type='info')
+                    self.flame.analysis_finished()
+                    aggregator.node_finished()  # LOOP BREAK
+                else:
+                    # Send aggregated result to analyzers
+                    self.flame.send_intermediate_data(analyzers, agg_res)
         else:
-            raise BrokenPipeError(_ERROR_MESSAGES.IS_ANALYZER.value)
+            raise BrokenPipeError(_ERROR_MESSAGES.IS_INCORRECT_CLASS.value)
