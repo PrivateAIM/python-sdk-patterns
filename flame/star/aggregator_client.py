3511d2ceba905f8a74248c856df1a8ad373b7482
davidhieber <david.hieber@uni-tuebingen.de>
2026-01-29 15:08:41 +0100

    feat: update aggregation methods to support multiple final results
    Co-authored-by: Nightknight3000 <alexander.roehl@uni-tuebingen.de>

--- flame/star/aggregator_client.py
+++ flame/star/aggregator_client.py
@@ -14,7 +14,7 @@ class Aggregator(Node):
             raise ValueError(f'Attempted to initialize aggregator node with mismatching configuration '
                              f'(expected: node_role="aggregator", received="{self.role}").')
 
-    def aggregate(self, node_results: list[Any], simple_analysis: bool = True) -> tuple[Any, bool, bool]:
+    def aggregate(self, node_results: list[Any], simple_analysis: bool = True) -> tuple[Union[Any, list[Any]], bool, bool]:
         result = self.aggregation_method(node_results)
 
         delta_criteria = self.has_converged(result, self.latest_result)
@@ -29,10 +29,10 @@ class Aggregator(Node):
         return self.latest_result, converged, delta_criteria
 
     @abstractmethod
-    def aggregation_method(self, analysis_results: list[Any]) -> Any:
+    def aggregation_method(self, analysis_results: list[Any]) -> Union[Any, list[Any]]:
         """
         This method will be used to aggregate the data. It has to be overwritten.
-        :return: aggregated_result
+        :return: aggregated_result - can be a single result or a list of results
         """
         pass
 
